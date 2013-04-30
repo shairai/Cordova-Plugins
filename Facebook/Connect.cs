a2770e8cda957c50d1281938cff45950be7a48a6
shairai <shai.raiten@gmail.com>
2013-04-30 06:56:31 +0300

    Update Connect.cs
    Remove Settings.Clear()

--- Facebook/Connect.cs
+++ Facebook/Connect.cs
@@ -20,8 +20,7 @@ namespace org.apache.cordova.facebook
     {
         public Connect()
         {
-            Settings = IsolatedStorageSettings.ApplicationSettings;
-            Settings.Clear();
+            Settings = IsolatedStorageSettings.ApplicationSettings;            
         }
 
         private static readonly DateTime Jan1st1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
